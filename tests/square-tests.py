3ea6400da4284504790a91ab099caee1c5406ccb
Volkov Daniil <volkov27daniil@yandex.ru>
2025-12-05 11:02:16 +0300

    tests: add tests for 'square.py' perimeter()

--- tests/square-tests.py
+++ tests/square-tests.py
@@ -13,3 +13,15 @@ class SquareTestCase(unittest.TestCase):
     def test_area_SideIsNegative(self):
         with self.assertRaises(ValueError):
             square.area(-1)
+
+    def test_perimeter_CorrectSide(self):
+        result = square.perimeter(5)
+        self.assertEqual(result, 20)
+
+    def test_perimeter_SideIsZero(self):
+        with self.assertRaises(ValueError):
+            square.perimeter(0)
+
+    def test_perimeter_SideIsNegative(self):
+        with self.assertRaises(ValueError):
+            square.perimeter(-1)
