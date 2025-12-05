ec3d826662685f95324ba9212b80573860364162
Volkov Daniil <volkov27daniil@yandex.ru>
2025-12-05 11:13:03 +0300

    tests: add tests for 'triangle.py' perimeter()

--- tests/triangle-tests.py
+++ tests/triangle-tests.py
@@ -29,3 +29,39 @@ class TriangleTestCase(unittest.TestCase):
     def test_area_SideAndHeightAreNegatives(self):
         with self.assertRaises(ValueError):
             triangle.area(-1, -1)
+
+    def test_perimeter_CorrectSides(self):
+        result = triangle.perimeter(3, 4, 5)
+        self.assertEqual(result, 12)
+
+    def test_perimeter_FirstSideIsZero(self):
+        with self.assertRaises(ValueError):
+            triangle.perimeter(0, 4, 5)
+
+    def test_perimeter_SecondSideIsZero(self):
+        with self.assertRaises(ValueError):
+            triangle.perimeter(3, 0, 5)
+
+    def test_perimeter_ThirdSideIsZero(self):
+        with self.assertRaises(ValueError):
+            triangle.perimeter(3, 4, 0)
+
+    def test_perimeter_AllSidesAreZero(self):
+        with self.assertRaises(ValueError):
+            triangle.perimeter(0, 0, 0)
+
+    def test_perimeter_FirstSideIsNegative(self):
+        with self.assertRaises(ValueError):
+            triangle.perimeter(-1, 4, 5)
+
+    def test_perimeter_SecondSideIsNegative(self):
+        with self.assertRaises(ValueError):
+            triangle.perimeter(3, -1, 5)
+
+    def test_perimeter_ThirdSideIsNegative(self):
+        with self.assertRaises(ValueError):
+            triangle.perimeter(3, 4, -1)
+
+    def test_perimeter_AllSidesAreNegative(self):
+        with self.assertRaises(ValueError):
+            triangle.perimeter(-1, -1, -1)
